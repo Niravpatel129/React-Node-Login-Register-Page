604dba95a01dd58cb3843c87d24336775d373ca6
Niravpatel129 <niravpatelp129@gmail.com>
2020-02-14 15:42:00 -0500

    redirect added

--- server/config/key.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  ClientID:
-    "690051817221-620ql6s9gcn6m9ktck2c12r1unv7ftp8.apps.googleusercontent.com",
-  ClientSecret: "Xzc7A06257oJWNTJ_sh1a6QS"
-};
--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    () => {}
+  )
+);
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
 
 const PORT = 5000;
 app.listen(PORT, () => {
